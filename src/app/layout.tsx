322dd13163d8969545b546bc876142a1793ded6c
QueensAuto <queensmobile11@gmail.com>
2026-01-02 14:25:27 -0600

    GTM

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -27,7 +27,7 @@ export default function RootLayout({
           new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
           j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
           'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
-          })(window,document,'script','dataLayer','GTM-W56FDLTF');`}} />
+          })(window,document,'script','dataLayer','GTM-NJBHC4TP');`}} />
         <link rel="preconnect" href="https://fonts.googleapis.com" />
         <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="anonymous" />
         <link href="https://fonts.googleapis.com/css2?family=Plus+Jakarta+Sans:wght@500;700;800&display=swap" rel="stylesheet" />
@@ -40,7 +40,7 @@ export default function RootLayout({
           'font-body antialiased bg-gradient-to-br from-black to-slate-950 text-gray-200 selection:bg-blue-500/20 selection:text-blue-300'
         )}
       >
-        <noscript><iframe src="https://www.googletagmanager.com/ns.html?id=GTM-W56FDLTF"
+        <noscript><iframe src="https://www.googletagmanager.com/ns.html?id=GTM-NJBHC4TP"
           height="0" width="0" style={{ display: 'none', visibility: 'hidden' }}></iframe></noscript>
         <LanguageProvider>
           {children}
